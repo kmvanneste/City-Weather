f5edd7d05ca82f0087a12b0e90db8542e88450ab
kmvanneste <kmvanneste17@gmail.com>
2020-08-03 18:53:17 -0400

    psuedo-code JS and adjusted sytlesheet

---
+++ script.js
@@ -0,0 +1,20 @@
+// SETUP VARIABLES
+//======================================================
+
+var authKey = "";
+
+
+
+// FUNCTIONS
+//======================================================
+
+//MAIN PROCESSES
+//======================================================
+
+
+//1. Retrieve user inputs and convert to variables
+//2. Use those variables to run on AJAX call to the weather website
+//3. Bread down the Weather Object into usable fields
+//4. Dynamically generate html content
+
+//5. Dealing with "edge cases" -- bugs or unusual situations that are not intuitive.
